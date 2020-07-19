6cdcdfed9a09ad1414dd58f148dafe8d3ddfb46f
moises <moises@moisess-iMac.local>
2020-07-19 13:57:36 -0500

    base

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:franklin/my_clipper.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,26 +11,81 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Consultorio Dental',
-      home: MyPage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyPage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-      slivers: <Widget>[
-        SliverAppBar(
-          pinned: true,
-          expandedHeight: 200,
-          flexibleSpace: Image.asset(
-            "assets/images/consultorio2.jpg",
-            fit: BoxFit.cover,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                      "assets/images/consultorio1.jpg",
+                    ),
+                    fit: BoxFit.fill)),
           ),
-        )
-      ],
-    ));
+          Expanded(
+              child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+                child: Text('data'),
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.red,
+                child: Text('data'),
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.red,
+                child: Text('data'),
+              ),Container(
+                width: 160.0,
+                color: Colors.red,
+                child: Text('data'),
+              )
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
+
+//class MyPage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//        body: CustomScrollView(
+//      slivers: <Widget>[
+//        SliverAppBar(
+//          pinned: true,
+//          expandedHeight: 200,
+//          flexibleSpace: Image.asset(
+//            "assets/images/consultorio2.jpg",
+//            fit: BoxFit.cover,
+//          ),
+//        ),
+//        SliverList(
+//
+//        )
+//      ],
+//    ));
+//  }
+//}
